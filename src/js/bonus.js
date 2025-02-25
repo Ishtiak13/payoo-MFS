e575d7275c24148a96587f6673ad773149207851
Ishtiak13 <ishtiakalhumaidi@gmail.com>
2025-02-25 20:49:22 +0600

    some changes

--- src/js/bonus.js
+++ src/js/bonus.js
@@ -5,26 +5,6 @@ function handleBonusClick() {
 
   if (couponNumber === "") {
     alert("Please Enter the Coupon!");
-  } else if (couponNumber === "SIAMCDI420") {
-    bonusForm.innerHTML = `
-      <section class="flex items-center justify-center flex-col gap-7">
-        <div role="alert" class="alert alert-vertical sm:alert-horizontal">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-black h-6 w-6 shrink-0">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-</svg>
-  <span>HaHa! Now You can do that with Siam. No Bonus Available..</span>
-</div>
-        <button id="try-again-btn" class="btn btn-wide mx-auto">Go Back</button>
-      </section>`;
-
-    document
-      .getElementById("try-again-btn")
-      .addEventListener("click", function () {
-        bonusForm.innerHTML = originalHtml;
-        document
-          .getElementById("btn-bonus")
-          .addEventListener("click", handleBonusClick);
-      });
   } else {
     bonusForm.innerHTML = `
       <section class="flex items-center justify-center flex-col gap-7">
@@ -34,12 +14,7 @@ function handleBonusClick() {
           </svg>
           <span>Invalid Coupon!</span>
         </div>
-        <div role="alert" class="alert alert-vertical sm:alert-horizontal">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-black h-6 w-6 shrink-0">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-</svg>
-  <span>You can use "SIAMCDI420" 😜.</span>
-</div>
+        
         <button id="try-again-btn" class="btn btn-wide mx-auto">Try Again</button>
       </section>`;
 
--- src/js/transfer.js
+++ src/js/transfer.js
@@ -26,7 +26,7 @@ function handleTransferClick() {
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
-  <span>Your money transfer Successfully!</span>
+  <span>Your money has been transfer Successfully!</span>
 </div>
 <button id="add-again-btn" class="btn btn-wide mx-auto">Transfer again</button>
             </section>`;
