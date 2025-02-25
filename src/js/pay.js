2f1d57fd798472c488db5456ac65cf359b351078
Ishtiak13 <ishtiakalhumaidi@gmail.com>
2025-02-25 23:15:42 +0600

    last commit

--- src/js/pay.js
+++ src/js/pay.js
@@ -21,7 +21,28 @@ function handlePayClick() {
         alert("Add valid amount!");
       } else if (pin == "1234") {
         if (balance === 0 || balance < money) {
-          alert("You have not enough balance to pay!");
+          document.getElementById("pay-bill-form").innerHTML = `
+            <section class="flex items-center justify-center flex-col gap-7">
+            <div role="alert" class="alert alert-error">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>You have not enough balance to pay! Check the balance.</span>
+</div>
+
+
+<button id="add-again-btn" class="btn btn-wide mx-auto">Try again</button>
+            </section>`;
+          document;
+          document
+            .getElementById("add-again-btn")
+            .addEventListener("click", function () {
+              document.getElementById("pay-bill-form").innerHTML =
+                originalFormHTML;
+              document
+                .getElementById("btn-pay-bill")
+                .addEventListener("click", handlePayClick);
+            });
         } else if (money > 25000) {
           alert("You Cannot Add More than 25000!");
         } else {
--- src/js/transfer.js
+++ src/js/transfer.js
@@ -18,7 +18,27 @@ function handleTransferClick() {
       alert("Add valid amount!");
     } else if (pin == "1234") {
       if (balance === 0 || balance < money) {
-        alert("You have not enough balance to transfer!");
+        document.getElementById("transfer-form").innerHTML = `
+        <section class="flex items-center justify-center flex-col gap-7">
+        <div role="alert" class="alert alert-error">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+<span>You have not enough balance to Transfer! Check the balance.</span>
+</div>
+
+
+<button id="add-again-btn" class="btn btn-wide mx-auto">Try again</button>
+        </section>`;
+        document
+          .getElementById("add-again-btn")
+          .addEventListener("click", function () {
+            document.getElementById("transfer-form").innerHTML =
+              originalFormHTML;
+            document
+              .getElementById("btn-transfer-money")
+              .addEventListener("click", handleTransferClick);
+          });
       } else if (money > 25000) {
         alert("You Cannot Withdraw More than 25000!");
       } else {
